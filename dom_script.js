64b6a79410d282899b3ebf45ff8196b378b73b40
Hai Tan <haitannyc@gmail.com>
2016-02-09 22:53:56 -0500

    fix js file to let console run

--- dom_script.js
+++ dom_script.js
@@ -7,18 +7,31 @@ $('form').submit(function() {
     alert('Username cannot be blank');
     return false;
 
-    // password must be "12345"
-
   } else if ($('#password').val() == "") {
     alert('Password cannot be blank');
     return false;
+
+   // password must be "12345" 
+
   } else if ($('#password').val() !== "12345") {
     alert('Wrong password');
     return false;
+
   } else {
-    alert('Welcome. You are logged in.');
+    alert('Log In Ok');
   }
 });
 
+// anything in body is typed--keypress
+
+$("body").on("keypress", function(e){
+      if(Number(e.keyCode) <= 57){
+        $("#change").html("number");
+      }
+      else {
+        $("#change").html("letter");
+      }
+      console.log(e)
+    });
 
   });
